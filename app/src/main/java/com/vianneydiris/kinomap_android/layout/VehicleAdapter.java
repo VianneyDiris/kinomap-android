f4a84bd4882802625e9f8ab80d66550f9e4790dd
VianneyDiris <vianney.diris@gmail.com>
2020-11-03 16:53:07 +0100

    add id after click

--- app/src/main/java/com/vianneydiris/kinomap_android/layout/VehicleAdapter.java
+++ app/src/main/java/com/vianneydiris/kinomap_android/layout/VehicleAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -15,30 +16,37 @@ import com.vianneydiris.kinomap_android.model.Vehicle;
 import java.util.List;
 
 public class VehicleAdapter  extends ArrayAdapter<Vehicle> {
+    private VehicleViewHolder viewHolder;
 
     public VehicleAdapter(Context context, List<Vehicle> vehicles) {
         super(context, 0, vehicles);
     }
 
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_vehicle,parent, false);
         }
 
-        VehicleViewHolder viewHolder = (VehicleViewHolder) convertView.getTag();
+        viewHolder = (VehicleViewHolder) convertView.getTag();
         if(viewHolder == null){
             viewHolder = new VehicleViewHolder();
             viewHolder.name = (TextView) convertView.findViewById(R.id.name);
             viewHolder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            viewHolder.id = (TextView) convertView.findViewById(R.id.id);
             convertView.setTag(viewHolder);
         }
 
         Vehicle vehicle = getItem(position);
         viewHolder.name.setText(vehicle.getName());
-        Picasso.get().load(vehicle.getIcon().getUrl().getSize50x50()).into( viewHolder.icon);
-      //  viewHolder.icon.setImageIcon(vehicle.getIcon().getUrl().getSize50x50());
+        Picasso.get().load(vehicle.getIcon().getUrl().getSize50x50()).into(viewHolder.icon);
+
+        VehicleViewHolder finalViewHolder = viewHolder;
+        convertView.setOnClickListener(v -> {
+            System.out.println(vehicle.getId());
+            viewHolder.id.setText(Integer.toString(vehicle.getId()));
+        });
 
         return convertView;
     }
@@ -46,6 +54,8 @@ public class VehicleAdapter  extends ArrayAdapter<Vehicle> {
     private class VehicleViewHolder{
         public TextView name;
         public ImageView icon;
+        public TextView id;
 
     }
+
 }
